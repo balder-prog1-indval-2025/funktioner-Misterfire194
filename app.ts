2bfe5ad5eb0fc1555b7200c683ad83e298ece692
Elliot <ebu0729@student.skelleftea.se>
2025-11-12 09:45:07 +0100

    Fortsatt arbete

--- app.ts
+++ app.ts
@@ -32,7 +32,7 @@ function dist2D (x1: number, y1: number, x2: number, y2: number) {
 return Math.sqrt((x2-x1)**2 + (y2-y1)**2)
 }
 write (dist2D(x1, y1, x2, y2))
-*/
+
 
 write("Uppgift 4:")
 let tal1 = +await read("Tal 1: ")
@@ -40,9 +40,39 @@ let tal2 = +await read("Tal 2: ")
 let tal3 = +await read("Tal 2: ")
 let tal4 = +await read("Tal 4: ")
 
-function ascending(tal1: number, tal2: number, tal3: number, tal4: number) {
+function ascending(tal1: number, tal2: number, tal3: number, tal4: number) { 
     if (tal1 <= tal2 && tal2 <= tal3 && tal3 <= tal4) {
-        true
-    } else {false}
+       write ("True")
+       return true;
+    } else { 
+        write ("False")
+        return false; 
+    } 
 }
-if (true) {write("true")}
+write(ascending(tal1, tal2, tal3, tal4))
+
+
+write ("Uppgift 5: ")
+let tal1 = +await read("Tal 1: ")
+let tal2 = +await read("Tal 2: ")
+let tal3 = +await read("Tal 2: ")
+let tal4 = +await read("Tal 4: ")
+
+let a = [tal1, tal2, tal3, tal4]
+write (a)
+function min (tal1:number, tal2:number, tal3:number, tal4:number) {
+    if (tal1 <= tal2 && tal1 <=tal3 && tal1<=tal4) { return write(tal1)}
+    else if (tal2<=tal1 && tal2<=tal3 && tal2<=tal4) { return write(tal2)}
+    else if (tal3<=tal1 && tal3<=tal2 && tal3<=tal4) { return write(tal3)}
+    else if (tal4<=tal1 && tal4<=tal3 && tal4<=tal2) { return write(tal4)}
+}
+write(min(tal1, tal2, tal3, tal4))
+*/
+
+write ("Uppgift 6: ") 
+function myFun(...manyMoreArgs) {
+    console.log("manyMoreArgs", manyMoreArgs);
+  }
+  
+  myFun("one", "two", "three", "four", "five", "six");
+  for (let i = 0; i<  ) {}  
